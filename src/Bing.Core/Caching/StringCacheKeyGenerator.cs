c6263f630c6221d1b7960256f79ddea2764d8b12
jian玄冰 <jianxuanhuo1@126.com>
2017-11-27 14:18:52 +0800

    加入缓存键，生成XML

---
+++ src/Bing.Core/Caching/StringCacheKeyGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Caching
+{
+    /// <summary>
+    /// 字符串缓存键生成器
+    /// </summary>
+    public class StringCacheKeyGenerator
+    {
+    }
+}
---
+++ src/Bing.Core/Datas/TransactionalBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Datas
+{
+    /// <summary>
+    /// 在执行数据库命令或查询期间控制事务创建行为
+    /// </summary>
+    public enum TransactionalBehavior
+    {
+        /// <summary>
+        /// 如果存在现有事务，则使用它，否则在没有事务的情况下执行命令或查询。
+        /// </summary>
+        DoNotEnsureTransaction,
+        /// <summary>
+        /// 如果不存在任何事务，则使用新事务进行操作。
+        /// </summary>
+        EnsureTransaction
+    }
+}
