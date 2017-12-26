9bc2a793b438716a4fe611b0e36c502394616f13
jian玄冰 <jianxuanhuo1@126.com>
2017-12-26 17:28:52 +0800

    实现树节点生成

--- src/Bing.Utils/Modes/Trees/Contexts/ITreeContext.cs
+++ src/Bing.Utils/Modes/Trees/Contexts/ITreeContext.cs
@@ -17,6 +17,16 @@ namespace Bing.Utils.Modes.Trees.Contexts
         /// </summary>
         ITreeNode Current { get; set; }
 
+        /// <summary>
+        /// 设置树节点列表数据源
+        /// </summary>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <param name="items">数据源</param>
+        /// <param name="textSelector">显示文本选择器</param>
+        /// <param name="idSelector">ID选择器</param>
+        /// <param name="parentIdSelector">上级ID选择器</param>
+        /// <param name="rootId">根ID</param>
+        /// <returns></returns>
         ITreeContext<T> SetItems<TKey>(List<T> items, Func<T, string> textSelector, Func<T, TKey> idSelector,
             Func<T, TKey> parentIdSelector, TKey rootId = default(TKey));
     }
--- src/Bing.Utils/Modes/Trees/Contexts/TreeContext.cs
+++ src/Bing.Utils/Modes/Trees/Contexts/TreeContext.cs
@@ -28,9 +28,20 @@ namespace Bing.Utils.Modes.Trees.Contexts
             this.Current = treeNode;
         }
 
+        /// <summary>
+        /// 设置树节点列表数据源
+        /// </summary>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <param name="items">数据源</param>
+        /// <param name="textSelector">显示文本选择器</param>
+        /// <param name="idSelector">ID选择器</param>
+        /// <param name="parentIdSelector">上级ID选择器</param>
+        /// <param name="rootId">根ID</param>
+        /// <returns></returns>
         public ITreeContext<T> SetItems<TKey>(List<T> items, Func<T,string> textSelector, Func<T, TKey> idSelector, Func<T, TKey> parentIdSelector, TKey rootId = default(TKey))
         {
-            var result = GenerateTree(items, textSelector, idSelector, parentIdSelector, rootId,Current.Level).ToList();            
+            var result = GenerateTree(items, textSelector, idSelector, parentIdSelector, Current, rootId, Current.Level)
+                .ToList();
             Current.Children = result;
             return this;
         }
@@ -38,16 +49,17 @@ namespace Bing.Utils.Modes.Trees.Contexts
         /// <summary>
         /// 生成树
         /// </summary>
-        /// <typeparam name="TKey"></typeparam>
-        /// <param name="collection"></param>
-        /// <param name="textSelector"></param>
-        /// <param name="idSelector"></param>
-        /// <param name="parentIdSelector"></param>
-        /// <param name="rootId"></param>
-        /// <param name="level"></param>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <param name="collection">数据源</param>
+        /// <param name="textSelector">显示文本选择器</param>
+        /// <param name="idSelector">ID选择器</param>
+        /// <param name="parentIdSelector">上级ID选择器</param>
+        /// <param name="parentNode">父节点</param>
+        /// <param name="rootId">根ID</param>
+        /// <param name="level">层级</param>
         /// <returns></returns>
         internal IEnumerable<ITreeNode> GenerateTree<TKey>(List<T> collection, Func<T, string> textSelector,
-            Func<T, TKey> idSelector, Func<T, TKey> parentIdSelector, TKey rootId = default(TKey),
+            Func<T, TKey> idSelector, Func<T, TKey> parentIdSelector,ITreeNode parentNode, TKey rootId = default(TKey),
             int? level = default(int))
         {
             level += 1;
@@ -61,8 +73,9 @@ namespace Bing.Utils.Modes.Trees.Contexts
                 node.Id = idSelector(c);
                 node.ParentId = parentIdSelector(c);
                 node.Level = level;
+                node.Path = parentNode.Level == 0 ? node.Text : string.Format("{0}.{1}", parentNode.Path, node.Text);
                 node.Children = GenerateTree<TKey>(collection, textSelector, idSelector, parentIdSelector,
-                    idSelector(c), level).ToList();
+                    node, idSelector(c), level).ToList();
                 yield return node;
             }
         }
