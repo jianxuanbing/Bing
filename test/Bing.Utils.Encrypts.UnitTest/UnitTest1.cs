47316d3330adb6f396f4fa1a26713d296723eba8
jian玄冰 <jianxuanhuo1@126.com>
2018-01-11 17:31:12 +0800

    加入 测试案例

---
+++ test/Bing.Utils.Encrypts.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Bing.Utils.Encrypts.Hash;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bing.Utils.Encrypts.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var key = "test001";
+            var result=SHACryptor.Sha1(key);
+            Console.WriteLine(result);
+        }
+    }
+}
