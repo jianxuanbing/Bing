2f31347b470a141887cabbeed836377e323212f4
jian玄冰 <jianxuanhuo1@126.com>
2018-01-13 17:18:20 +0800

    二进制

---
+++ src/Bing.Utils/Maths/HexConv.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Utils.Maths
+{
+    /// <summary>
+    /// 进制转换
+    /// </summary>
+    public class HexConv
+    {
+        /// <summary>
+        /// 二进制转换为八进制
+        /// </summary>
+        /// <param name="value">二进制</param>
+        /// <returns></returns>
+        public static string BinToOct(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 二进制转换为十进制
+        /// </summary>
+        /// <param name="value">二进制</param>
+        /// <returns></returns>
+        public static string BinToDec(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 二进制转换为十六进制
+        /// </summary>
+        /// <param name="value">二进制</param>
+        /// <returns></returns>
+        public static string BinToHex(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 八进制转换为二进制
+        /// </summary>
+        /// <param name="value">八进制</param>
+        /// <returns></returns>
+        public static string OctToBin(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 八进制转换为十进制
+        /// </summary>
+        /// <param name="value">八进制</param>
+        /// <returns></returns>
+        public static string OctToDec(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 八进制转换为十六进制
+        /// </summary>
+        /// <param name="value">八进制</param>
+        /// <returns></returns>
+        public static string OctToHex(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 十进制转换为二进制
+        /// </summary>
+        /// <param name="value">十进制</param>
+        /// <returns></returns>
+        public static string DecToBin(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 十进制转换为八进制
+        /// </summary>
+        /// <param name="value">十进制</param>
+        /// <returns></returns>
+        public static string DecToOct(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 十进制转换为十六进制
+        /// </summary>
+        /// <param name="value">十进制</param>
+        /// <returns></returns>
+        public static string DecToHex(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 十六进制转换为二进制
+        /// </summary>
+        /// <param name="value">十六进制</param>
+        /// <returns></returns>
+        public static string HexToBin(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 十六进制转换为八进制
+        /// </summary>
+        /// <param name="value">十六进制</param>
+        /// <returns></returns>
+        public static string HexToOct(string value)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 十六进制转换为十进制
+        /// </summary>
+        /// <param name="value">十六进制</param>
+        /// <returns></returns>
+        public static string HexToDec(string value)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ src/Bing.Utils/Maths/UnitConv.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Utils.Maths
+{
+    /// <summary>
+    /// 单位转换
+    /// </summary>
+    public class UnitConv
+    {
+    }
+}
