ffd14242a3a26f23841d598257b7e7f3e4a79cf0
jian玄冰 <jianxuanhuo1@126.com>
2018-01-24 14:20:22 +0800

    加入测试

---
+++ test/Bing.Utils.AutoMapper.UnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bing.Utils.Json;
+
+namespace Bing.Utils.AutoMapper.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void Init()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var user=new UserInfoDto()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test001"
+            };
+            var result = user.MapTo<UserInfo>();
+            Console.WriteLine(result.ToJson());
+        }
+    }
+}
---
+++ test/Bing.Utils.AutoMapper.UnitTest/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Utils.AutoMapper.UnitTest
+{
+    /// <summary>
+    /// 用户信息
+    /// </summary>
+    public class UserInfo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid? GroupId { get; set; }
+    }
+
+    /// <summary>
+    /// 用户信息 数据传输对象
+    /// </summary>
+    public class UserInfoDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid? GroupId { get; set; }
+    }
+}
