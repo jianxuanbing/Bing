1d9b374408a98346f8cded352d4518135ce2f4c4
jian玄冰 <jianxuanhuo1@126.com>
2018-02-27 00:11:26 +0800

    扩展 对象验证

--- src/Bing.Utils/Extensions/Common/Extensions.Validate.cs
+++ src/Bing.Utils/Extensions/Common/Extensions.Validate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bing.Utils.Extensions
@@ -11,7 +12,7 @@ namespace Bing.Utils.Extensions
     {
         #region IsEmpty(是否为空)
         /// <summary>
-        /// 字符串是否为空、null或空白字符串
+        /// 字符串 是否为空、null或空白字符串
         /// </summary>
         /// <param name="value">数据</param>
         /// <returns></returns>
@@ -21,7 +22,7 @@ namespace Bing.Utils.Extensions
         }
 
         /// <summary>
-        /// Guid是否为空、null或Guid.Empty
+        /// Guid 是否为空、null或Guid.Empty
         /// </summary>
         /// <param name="value">数据</param>
         /// <returns></returns>
@@ -35,7 +36,7 @@ namespace Bing.Utils.Extensions
         }
 
         /// <summary>
-        /// Guid是否为空、null或Guid.Empty
+        /// Guid 是否为空、null或Guid.Empty
         /// </summary>
         /// <param name="value">数据</param>
         /// <returns></returns>
@@ -49,13 +50,57 @@ namespace Bing.Utils.Extensions
         }
 
         /// <summary>
-        /// 数组是否为空
+        /// 数组 是否为空
         /// </summary>
         /// <param name="array">数据</param>
         /// <returns></returns>
         public static bool IsEmpty(this Array array)
         {
             return array == null || array.Length == 0;
+        }
+
+        /// <summary>
+        /// 可变字符串 是否为空
+        /// </summary>
+        /// <param name="sb">数据</param>
+        /// <returns></returns>
+        public static bool IsEmpty(this StringBuilder sb)
+        {
+            return sb == null || sb.Length == 0 || sb.ToString().IsEmpty();
+        }
+
+        /// <summary>
+        /// 泛型集合 是否为空
+        /// </summary>
+        /// <typeparam name="T">泛型对象</typeparam>
+        /// <param name="list">数据</param>
+        /// <returns></returns>
+        public static bool IsEmpty<T>(this ICollection<T> list)
+        {
+            return null == list || list.Count == 0;
+        }
+
+        /// <summary>
+        /// 迭代集合 是否为空
+        /// </summary>
+        /// <typeparam name="T">泛型对象</typeparam>
+        /// <param name="list">数据</param>
+        /// <returns></returns>
+        public static bool IsEmpty<T>(this IEnumerable<T> list)
+        {
+            return null == list || !list.Any();
+        }
+
+        /// <summary>
+        /// 字典 是否为空
+        /// </summary>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <typeparam name="TValue">值类型</typeparam>
+        /// <param name="dictionary">数据</param>
+        /// <returns></returns>
+        public static bool IsEmpty<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return null == dictionary || dictionary.Count == 0;
         }
         #endregion
 
