639b415c932b3b5b7ae8d96945dc8bae023fea0e
jian玄冰 <jianxuanhuo1@126.com>
2018-05-18 10:44:00 +0800

    加入 存储过程查询

--- src/Bing.Datas.Extensions.Dapper/RepositoryExtensions.cs
+++ src/Bing.Datas.Extensions.Dapper/RepositoryExtensions.cs
@@ -93,8 +93,77 @@ namespace Bing.Datas.Extensions.Dapper
             IDbTransaction transaction = null, int? commandTimeout = null)
         {
             var connection = repository.GetDbConnection();
-            OrmConfig.AdoLogInterceptor?.Invoke("ExecuteProc", procName, param);
+            OrmConfig.AdoLogInterceptor?.Invoke("ExecuteProcAsync", procName, param);
             return await connection.ExecuteAsync(procName, param, transaction, commandTimeout, CommandType.StoredProcedure);
+        }
+
+        #endregion
+
+        #region ProcQuery(通过存储过程执行数据查询操作，返回集合)
+
+        /// <summary>
+        /// 通过存储过程执行数据查询操作，返回集合
+        /// </summary>
+        /// <typeparam name="TResult">对象类型</typeparam>
+        /// <param name="repository">仓储</param>
+        /// <param name="procName">存储过程名</param>
+        /// <param name="param">存储过程参数，使用匿名对象传入，范例：new { Name = "A" }</param>
+        /// <param name="transaction">事务</param>
+        /// <param name="commandTimeout">命令超时时间</param>
+        /// <returns></returns>
+        public static IEnumerable<TResult> ProcQuery<TResult>(this IRepository repository, string procName,
+            object param = null, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            var connection = repository.GetDbConnection();
+            OrmConfig.AdoLogInterceptor?.Invoke("ProcQuery", procName, param);
+            return connection.Query<TResult>(procName, param, transaction, commandTimeout: commandTimeout,
+                commandType: CommandType.StoredProcedure);
+        }
+
+        #endregion
+
+        #region ProcQueryAsync(通过存储过程执行数据查询操作，返回集合)
+
+        /// <summary>
+        /// 通过存储过程执行数据查询操作，返回集合
+        /// </summary>
+        /// <typeparam name="TResult">对象类型</typeparam>
+        /// <param name="repository">仓储</param>
+        /// <param name="procName">存储过程名</param>
+        /// <param name="param">存储过程参数，使用匿名对象传入，范例：new { Name = "A" }</param>
+        /// <param name="transaction">事务</param>
+        /// <param name="commandTimeout">命令超时时间</param>
+        /// <returns></returns>
+        public static async Task<IEnumerable<TResult>> ProcQueryAsync<TResult>(this IRepository repository, string procName,
+            object param = null, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            var connection = repository.GetDbConnection();
+            OrmConfig.AdoLogInterceptor?.Invoke("ProcQueryAsync", procName, param);
+            return await connection.QueryAsync<TResult>(procName, param, transaction, commandTimeout: commandTimeout,
+                commandType: CommandType.StoredProcedure);
+        }
+
+        #endregion
+
+        #region ProcSingle(通过存储过程执行数据查询操作，返回对象)
+
+        /// <summary>
+        /// 通过存储过程执行数据查询操作，返回对象
+        /// </summary>
+        /// <typeparam name="TResult">对象类型</typeparam>
+        /// <param name="repository">仓储</param>
+        /// <param name="procName">存储过程名</param>
+        /// <param name="param">存储过程参数，使用匿名对象传入，范例：new { Name = "A" }</param>
+        /// <param name="transaction">事务</param>
+        /// <param name="commandTimeout">命令超时时间</param>
+        /// <returns></returns>
+        public static TResult ProcSingle<TResult>(this IRepository repository, string procName,
+            object param = null, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            var connection = repository.GetDbConnection();
+            OrmConfig.AdoLogInterceptor?.Invoke("ProcSingle", procName, param);
+            return connection.QueryFirstOrDefault<TResult>(procName, param, transaction, commandTimeout: commandTimeout,
+                commandType: CommandType.StoredProcedure);
         }
 
         #endregion
